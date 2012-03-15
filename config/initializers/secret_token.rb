d5a2fa080a03e95ad1ee25ee74229cf23f5fc2e1
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-03-15 13:41:29 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pawned::Application.config.secret_token = '246c36a1aa361f2faff319394696ccf5b435b72ba5a65b4d3c94f1120bc3b39dd8b3340a718d2bb8a77ae624ec62276da7f827f4fc9cf6efda5d5a8e507772b8'
