8b2d264ae897023c5ab6d072ecb6d83e949f0ef3
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-03-18 12:51:36 +0000

    Remove :confirmable from the Player model.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :encryptable, :confirmable, :lockable, :timeoutable
+         :encryptable, :lockable, :timeoutable
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
