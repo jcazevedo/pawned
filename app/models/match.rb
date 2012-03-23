01e3d47b9ebcbc139af0e2d58cab087efe7d8219
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-03-23 00:56:03 +0000

    Obsessive-compulsive style fix in match.rb.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,5 @@
 class Match < ActiveRecord::Base
-	belongs_to :white_id, :class_name =>"Player"
-        belongs_to :black_id, :class_name =>"Player"
-	has_one :round
+  belongs_to :white_id, :class_name =>"Player"
+  belongs_to :black_id, :class_name =>"Player"
+  has_one :round
 end
