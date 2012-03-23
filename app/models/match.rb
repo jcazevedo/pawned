2e2340e9818f0b597a84deb710b35b0f005b68e6
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-03-23 01:13:14 +0000

    Add virtual attribute result to Match model.

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,14 @@ class Match < ActiveRecord::Base
   belongs_to :white_player, :class_name => "Player", :foreign_key => "white_id" 
   belongs_to :black_player, :class_name => "Player", :foreign_key => "black_id"
   has_one :round
+
+  def result
+    [white_result.to_s, black_result.to_s].join('-')
+  end
+
+  def result=(result)
+    split = result.split('-', 2)
+    self.white_result = split.first.to_f
+    self.black_result = split.last.to_f
+  end
 end
