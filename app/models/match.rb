220cc3d1b8a2e36daf5c7ace184187eab873bdd0
Luis Carvalho <mail.lmcarvalho@gmail.com>
2012-03-24 17:06:01 +0000

    New layout. Dashboard now with tournament collapse.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ActiveRecord::Base
-  belongs_to :white_player, :class_name => "Player", :foreign_key => "white_id" 
+  belongs_to :white_player, :class_name => "Player", :foreign_key => "white_id"
   belongs_to :black_player, :class_name => "Player", :foreign_key => "black_id"
-  has_one :round
+  belongs_to :round
 
   def result
     [white_result.to_s, black_result.to_s].join('-')
