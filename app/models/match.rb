2ac66b551a8cb9664004d59ae11d9e8ffef03423
Luis Carvalho <mail.lmcarvalho@gmail.com>
2012-04-03 09:31:56 +0100

    Reactivated date and players validations on matches.

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,8 +7,8 @@ class Match < ActiveRecord::Base
   has_one :round, :through => :duel
 
   validates :white_id, :black_id, :presence => true
-#  validates_with MatchDateValidator
-#  validates_with MatchPlayersValidator
+  validates_with MatchDateValidator
+  validates_with MatchPlayersValidator
 
   after_save :check_for_rating_update
 
